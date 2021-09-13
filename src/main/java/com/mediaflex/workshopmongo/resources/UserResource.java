5baa25295bfac3263dce113305a58591167cc8c3
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-13 10:18:20 -0300

    insert usuário post

--- src/main/java/com/mediaflex/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mediaflex/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.mediaflex.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mediaflex.workshopmongo.domain.User;
 import com.mediaflex.workshopmongo.dto.UserDTO;
@@ -40,4 +43,12 @@ public class UserResource  {
 		return ResponseEntity.ok().body( new UserDTO(obj) );
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		// retorno vazio com cabeçalho com url do novo recurso criado 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
