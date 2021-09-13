ddb8fb73e58b146ebb221441eb9eea92ddd77872
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-13 10:26:58 -0300

    implementacao delete

--- src/main/java/com/mediaflex/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mediaflex/workshopmongo/resources/UserResource.java
@@ -51,4 +51,11 @@ public class UserResource  {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		// retornar código 204
+		return ResponseEntity.noContent().build();
+	}
 }
